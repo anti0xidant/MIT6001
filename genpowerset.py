be52b0a9c9ea94e1f197acb5a2a62da72a20b299
Andrew Tang <atang.business@gmail.com>
2016-02-22 23:08:24 -0600

    added genpowerset with test cases

---
+++ genpowerset.py
@@ -0,0 +1,130 @@
+def genPowerset(L):
+    '''
+    Assumes L is a list.
+    Returns a list of lists that contains all possible
+    combinations of the elements of L. E.g., if
+    L is [1, 2] it will return a list with elements
+    [], [1], [2], [1, 2].
+    '''
+    powerset = []
+    #For every number in the range 0 to 2^len(L):
+    for n in range(2**len(L)):
+
+        #Convert that number to binary string
+        binaryStr = getBinaryRep(n, len(L))
+
+        #Create a temp subset:
+        subset = []
+        
+        #For every position in binary string:
+        for i in range(len(binaryStr)):
+
+            #If that position is equal to 1:
+            if binaryStr[i] == '1':
+
+                #Store the parallel position of L in temp list
+                subset.append(L[i])
+                
+        #Add that list to powerset
+        powerset.append(subset)
+
+    #Return powerset
+    return powerset
+
+
+def getBinaryRep(n, numDigits):
+    '''
+    Assumes n and numDigits are non-negative ints.
+    Returns a numDigits str that is a binary
+    representation of n.
+    '''
+    result = ''
+    #While n is greater than zero:
+    while n > 0:
+        
+        #Append the least significant digit
+        leastSignificant = n % 2
+        result = str(leastSignificant) + result
+
+        #Slice off the least significant digit
+        n = n / 2
+
+    #If numDigits is less than length of binary string:
+    if numDigits < len(result):
+
+        #Raise ValueError:
+        raise ValueError('numDigits: Inappropriate value')
+        
+    #Add in enough 0's to make length equal to numDigits
+    missingZeroes = numDigits - len(result)
+    result = missingZeroes*'0' + result
+
+    #Return result
+    return result
+
+def tGetBinaryRep():
+    #case[0] = n
+    #case[1] = numDigits
+    #case[2] = expected
+    cases = ((0, 0, ''),
+             (0, 2, '00'),
+             (0, 4, '0000'),
+             (1, 1, '1'),
+             (1, 2, '01'),
+             (1, 4, '0001'),
+             (3, 2, '11'),
+             (3, 4, '0011'),
+             (8, 4, '1000'),
+             (14, 4, '1110'))
+
+    print 'Testing getBinaryRep()'
+    print 'Number of test cases:', len(cases)
+
+    for i in range(len(cases)):
+        expected = cases[i][2]
+        actual = getBinaryRep(cases[i][0], cases[i][1])
+
+        if expected == actual:
+            result = 'Success'
+        else:
+            result = 'FAILURE'
+
+        print 'Test', i+1, '-', result
+        print '    expected:', expected
+        print '    actual:', actual
+
+    print 'End of tests.'
+        
+def tGenPowerset():
+    #case[0] = L
+    #case[1] = expected
+    cases = (([], [[]]),
+              ([1], [[], [1]]),
+              ([1,2], [[], [1], [2], [1, 2]]),
+              ([1, 2, 3], [[], [1], [2], [3], [1, 2], [1, 3], [2, 3], [1, 2, 3]]))
+
+    print 'Testing genPowerset()'
+    print 'Number of test cases:', len(cases)
+
+    for i in range(len(cases)):
+        expected = cases[i][1]
+        actual = genPowerset(cases[i][0])
+
+        if len(expected) != len(actual):
+            result = 'FAILURE'
+        else:
+            for a in actual:
+                for e in expected:
+                    if a == e:
+                        matched = True
+                        break
+                if not matched:
+                    result = 'FAILURE'
+                    break
+            result = 'Success'
+
+        print 'Test', i+1, '-', result
+        print '    expected:', expected
+        print '    actual:', actual
+
+    print 'End of tests.'
