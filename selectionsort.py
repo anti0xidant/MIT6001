366bd5d8add1735eb2520bd7f18bcac2869c1c25
Andrew Tang <atang.business@gmail.com>
2016-02-24 15:15:42 -0600

    removed debugging print statements from selsort.py

--- selectionsort.py
+++ selectionsort.py
@@ -7,16 +7,15 @@ def selSort(L):
     firstElement = 0
     #While suffix still contains elements:
     while firstElement != len(L):
-        print 'entered loop'
+
         #For ever element e in the suffix:
         for i in range(firstElement, len(L)):
 
             #If e is smaller than first element f of suffix:
             if L[i] < L[firstElement]:
-                print 'found smaller'
+
                 #Swap positions between e and f
                 L[i], L[firstElement] = L[firstElement], L[i]
-        print L
 
         #Append f to the prefix
         firstElement = firstElement + 1
