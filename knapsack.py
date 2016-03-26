d4a8210e5bacad7d6432510bcbbc7127c092a528
Andrew Tang <atang.business@gmail.com>
2016-03-26 16:45:08 -0500

    implemented binaryNode

--- knapsack.py
+++ knapsack.py
@@ -4,7 +4,25 @@
 #########
 
 class binaryNode(object, value):
-    raise NotImplementedError
+    def __init__(self, value):
+        self.value = value
+        self.leftNode = None
+        self.rightNode = None
+        self.parentNode = None
+    def setLeft(self, node):
+        self.leftNode = node
+        node.parentNode = self
+    def getLeft(self):
+        return self.leftNode
+    def setRight(self, node):
+        self.rightNode = node
+        node.parentNode = self
+    def getRight(self):
+        return self.rightNode
+    def getParent(self):
+        return self.parent
+    def __str__(self):
+        return str(self.value)
 
 def buildTree(current, todo):
     raise NotImplementedError
